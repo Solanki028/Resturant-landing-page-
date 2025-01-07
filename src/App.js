ce81828e836a2454ff82d5cdbf7082f39a7d458a
unknown <you@example.com>
2025-01-07 21:40:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-// import Resturant from "./component/Basics/Resturant";cd 
-//  import UseState from "./component/Basics/Hooks/useState";
-// import UseEffect from "./component/Hooks/useEffect";
-// import UseReducer from "./component/Hooks/useReducer";
- import Resturant from "./component/Basics/Resturant";
-// import UseReducer from "./component/Basics/Hooks/useReducer";
+import Todo from "./component/Basics/todoreact/todo";
+// import Temp from "./components/weather/temp";
 
 const App = () => {
-  return <Resturant />;
+  return (
+    <>
+      <Todo />
+    </>
+  );
 };
 
 export default App;
