7dbcecaf9072df220da42160fae035583adc7a50
unknown <you@example.com>
2025-01-07 15:58:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import Resturant from "./component/Basics/Resturant";cd 
+//  import UseState from "./component/Basics/Hooks/useState";
+// import UseEffect from "./component/Hooks/useEffect";
+// import UseReducer from "./component/Hooks/useReducer";
+ import Resturant from "./component/Basics/Resturant";
+// import UseReducer from "./component/Basics/Hooks/useReducer";
+
+const App = () => {
+  return <Resturant />;
+};
+
+export default App;
